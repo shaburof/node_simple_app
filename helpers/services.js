6978cd03b59ae6af12f640f99248a7df0d8a32cd
shaburov <shaburof@gmail.com>
2021-12-21 14:36:01 +0500

    change ip address render

--- helpers/services.js
+++ helpers/services.js
@@ -6,12 +6,23 @@ const getMainColor = function (mainThemeColor, color) {
 }
 
 const getLocalIpAddress = function () {
-    let ip = '';
+    let ip = [];
+
     for (const value in networkInterfaces) {
-        ip += networkInterfaces[value][0]['address'] + ', ';
+        ip.push(networkInterfaces[value][0]['address']);
     }
 
-    return ip.slice(0, ip.length - 2);
+    return ip;
+}
+
+const arrayToList = function (arr) {
+    let html = '<ul style="color: white;">';
+    for (const item of arr) {
+        html += `<li>${item}</li>`
+    }
+    html += '</ul>';
+
+    return html;
 }
 
 const getVariablesList = function () {
@@ -30,7 +41,7 @@ module.exports.html = function (mainThemeColor, mainColor) {
     let html = `<body style="background-color: ${getMainColor(mainThemeColor, mainColor)};">`
     html += '<h1 style="color: white;">Node Express application</h1>';
     html += '<h3 style="color: white;">version: 9.0</h3>';
-    html += '<p style="color: white;">local ip address: ' + getLocalIpAddress() + '</p>';
+    html += '<p style="color: white;">local ip address: ' + arrayToList(getLocalIpAddress()) + '</p>';
     html += '<hr/>';
     html += '<p>environment variables list:</p>';
     html += getVariablesList();
